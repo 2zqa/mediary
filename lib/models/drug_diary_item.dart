8489c73f26233eac3c867f0e310d4795164e2af3
2zqa <25235249+2zqa@users.noreply.github.com>
2023-09-28 18:36:22 +0200

    Replace amount and unit with string, fix json

--- lib/models/drug_diary_item.dart
+++ lib/models/drug_diary_item.dart
@@ -3,36 +3,10 @@ import 'package:uuid/uuid.dart';
 
 const _uuid = Uuid();
 
-enum Unit {
-  mg,
-  ug,
-  g,
-  mL,
-  L,
-}
-
-Unit parseUnit(String unitString) {
-  switch (unitString) {
-    case 'mg':
-      return Unit.mg;
-    case 'ug':
-      return Unit.ug;
-    case 'g':
-      return Unit.g;
-    case 'mL':
-      return Unit.mL;
-    case 'L':
-      return Unit.L;
-    default:
-      throw ArgumentError('Invalid unit string: $unitString');
-  }
-}
-
 class DrugDiaryItem implements Comparable<DrugDiaryItem> {
   final String id;
   final String name;
-  final double amount;
-  final Unit unit;
+  final String amount;
   final DateTime date;
   final String? notes;
 
@@ -40,7 +14,6 @@ class DrugDiaryItem implements Comparable<DrugDiaryItem> {
     String? id,
     required this.name,
     required this.amount,
-    required this.unit,
     required this.date,
     this.notes,
   })  : assert(id == null || Uuid.isValidUUID(fromString: id)),
@@ -49,8 +22,7 @@ class DrugDiaryItem implements Comparable<DrugDiaryItem> {
   factory DrugDiaryItem.fromJson(Map<String, dynamic> json) {
     return DrugDiaryItem(
       name: json['drug'],
-      amount: double.parse(json['amount']),
-      unit: parseUnit(json['unit']),
+      amount: json['amount'],
       date: DateTime.parse(json['date']),
       notes: json['notes'],
     );
@@ -58,10 +30,9 @@ class DrugDiaryItem implements Comparable<DrugDiaryItem> {
 
   Map<String, dynamic> toJson() {
     return {
-      'drug': name,
+      'name': name,
       'amount': amount,
-      'unit': unit,
-      'date': date,
+      'date': date.toIso8601String(),
       'notes': notes,
     };
   }
