1f21f8edb870ad8ed89f3dedb22a3767aa08223c
2zqa <25235249+2zqa@users.noreply.github.com>
2023-09-28 18:30:55 +0200

    Remove unneeded backgroundColor specification

--- lib/routes/add_drug/add_drug.dart
+++ lib/routes/add_drug/add_drug.dart
@@ -7,7 +7,6 @@ class AddDrugRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Add Drug Use'),
       ),
       body: const Center(
