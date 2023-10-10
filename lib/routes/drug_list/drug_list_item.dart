9aeef37f22c7c4bcdc8a951d04f15dd1cd3b5cf3
2zqa <25235249+2zqa@users.noreply.github.com>
2023-10-11 01:39:17 +0200

    Remove unused import

--- lib/routes/drug_list/drug_list_item.dart
+++ lib/routes/drug_list/drug_list_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mediary/formatting/date_formatter.dart';
 import 'package:mediary/models/drug_entry.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
