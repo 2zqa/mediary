f36adeed2f1cea82057293ac6d0d3f0a419fd3fa
2zqa <25235249+2zqa@users.noreply.github.com>
2023-10-17 17:06:39 +0200

    Pretty print date in calendar view

--- lib/formatting/date_formatter.dart
+++ lib/formatting/date_formatter.dart
@@ -1,9 +1,13 @@
 import 'package:intl/intl.dart';
 
-formatDate(DateTime dateTime, String localeName) {
+String formatDate(DateTime dateTime, String localeName) {
   return DateFormat.yMMMMEEEEd(localeName).format(dateTime);
 }
 
-formatDateTime(DateTime dateTime, String localeName) {
+String formatMonthYear(DateTime dateTime, String localeName) {
+  return DateFormat.yMMMM(localeName).format(dateTime);
+}
+
+String formatDateTime(DateTime dateTime, String localeName) {
   return DateFormat.yMMMEd(localeName).add_jm().format(dateTime);
 }
--- lib/routes/drug_calendar_view/drug_calendar_view.dart
+++ lib/routes/drug_calendar_view/drug_calendar_view.dart
@@ -1,6 +1,8 @@
 import 'package:calendar_view/calendar_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mediary/formatting/date_formatter.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../models/drug_entry.dart';
 import '../../providers/drug_entries_provider.dart';
@@ -13,12 +15,15 @@ class DrugCalendarView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final AsyncValue<EventController<DrugEntry>> controller =
         ref.watch(drugCalendarEntriesProvider);
+    final locale = AppLocalizations.of(context)!.localeName;
     return controller.when(
       loading: () => const Center(child: CircularProgressIndicator()),
       error: (error, stack) => Center(child: Text('Error: $error')),
       data: (controller) => MonthView(
         controller: controller,
-        onEventTap: (eventData, __) {
+        headerStringBuilder: (date, {secondaryDate}) =>
+            formatMonthYear(date, locale),
+        onEventTap: (eventData, _) {
           final drug = eventData.event;
           if (drug == null) return;
           Navigator.push(
