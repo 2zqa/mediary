ce935cfa1d08b3faf4cee4b76923587c1612a82d
2zqa <25235249+2zqa@users.noreply.github.com>
2023-10-18 13:47:56 +0200

    Use theme colors, support dark mode in calendar view

--- lib/routes/drug_calendar_view/drug_calendar_view.dart
+++ lib/routes/drug_calendar_view/drug_calendar_view.dart
@@ -14,6 +14,8 @@ class DrugCalendarView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final colorScheme = Theme.of(context).colorScheme;
+
     final AsyncValue<EventController<DrugEntry>> controller =
         ref.watch(drugCalendarEntriesProvider);
     final locale = AppLocalizations.of(context)!.localeName;
@@ -22,6 +24,7 @@ class DrugCalendarView extends ConsumerWidget {
       error: (error, stack) => Center(child: Text('Error: $error')),
       data: (controller) => MonthView(
         controller: controller,
+        borderColor: colorScheme.outlineVariant,
         useAvailableVerticalSpace: true,
         headerStringBuilder: (date, {secondaryDate}) =>
             formatMonthYear(date, locale),
@@ -33,16 +36,37 @@ class DrugCalendarView extends ConsumerWidget {
             MaterialPageRoute(builder: (_) => AddDrugForm(initialDate: date)),
           );
         },
-        onEventTap: (eventData, _) {
-          final drug = eventData.event;
-          if (drug == null) return;
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => DrugDetails(drug),
-            ),
-          );
-        },
+        headerBuilder: (date) => MonthPageHeader(
+          date: date,
+          backgroundColor: colorScheme.primaryContainer,
+          iconColor: colorScheme.onPrimaryContainer,
+        ),
+        weekDayBuilder: (day) => WeekDayTile(
+          dayIndex: day,
+          backgroundColor: colorScheme.background,
+          textStyle: Theme.of(context).textTheme.bodyMedium,
+          // It is currently not supported to specify custom colors for the
+          // weekday borders, so disable it for now.
+          displayBorder: false,
+        ),
+        cellBuilder: (date, events, isToday, isInMonth) =>
+            FilledCell<DrugEntry>(
+          date: date,
+          shouldHighlight: isToday,
+          titleColor: colorScheme.onBackground,
+          backgroundColor: colorScheme.background,
+          events: events,
+          onTileTap: (eventData, _) {
+            final drug = eventData.event;
+            if (drug == null) return;
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => DrugDetails(drug),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
