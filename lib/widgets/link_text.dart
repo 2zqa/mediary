5b92e60c897dbdc309246aeb8c16fbc616d61d89
2zqa <25235249+2zqa@users.noreply.github.com>
2023-10-26 16:00:22 +0200

    Add LinkText widget

---
+++ lib/widgets/link_text.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class LinkText extends StatelessWidget {
+  final String text;
+  final String? uriText;
+  final Uri uri;
+  final TextStyle? style;
+  final void Function()? onFailLaunchUri;
+
+  const LinkText(
+    this.text, {
+    super.key,
+    this.uriText,
+    required this.uri,
+    this.onFailLaunchUri,
+    this.style,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final realUriText = uriText ?? uri.toString();
+    final beforeUriText = text.substring(0, text.indexOf(realUriText));
+    final afterUriText =
+        text.substring(text.indexOf(realUriText) + realUriText.length);
+
+    final DefaultTextStyle defaultTextStyle = DefaultTextStyle.of(context);
+    final TextStyle effectiveTextStyle = style ?? defaultTextStyle.style;
+
+    return RichText(
+      text: TextSpan(
+        style: effectiveTextStyle,
+        children: [
+          TextSpan(text: beforeUriText),
+          TextSpan(
+            text: realUriText,
+            style: effectiveTextStyle.copyWith(
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () async {
+                final canLaunch = await canLaunchUrl(uri);
+                if (!canLaunch) {
+                  onFailLaunchUri?.call();
+                  return;
+                }
+                await launchUrl(uri);
+              },
+          ),
+          TextSpan(text: afterUriText),
+        ],
+      ),
+    );
+  }
+}
