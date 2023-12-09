018dcc4eac2ea43e3d6b5314b6d637fffef7a9f6
2zqa <25235249+2zqa@users.noreply.github.com>
2023-12-09 20:25:12 +0100

    fix migration

--- lib/migrations/drug_entry_migrations.dart
+++ lib/migrations/drug_entry_migrations.dart
@@ -4,7 +4,7 @@ class DrugEntriesDatabase {
 
   static final List<String> migrations = [
     'CREATE TABLE $_tableName(id TEXT PRIMARY KEY, name TEXT NOT NULL, amount TEXT NOT NULL, date TEXT NOT NULL, notes TEXT)',
-    'ALTER TABLE $_tableName ADD COLUMN color integer',
+    'ALTER TABLE $_tableName ADD COLUMN color_index INTEGER NOT NULL DEFAULT 0',
   ];
 
   static int get version => DrugEntriesDatabase.migrations.length;
