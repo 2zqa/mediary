5013187a2683626d4a305ce0a4ad7577af650ac9
2zqa <25235249+2zqa@users.noreply.github.com>
2024-02-03 14:25:50 +0100

    Add delete button to detail page

--- lib/routes/drug_details/drug_details.dart
+++ lib/routes/drug_details/drug_details.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../formatting/date_formatter.dart';
 import '../../models/drug_entry.dart';
+import '../../providers/drug_entries_provider.dart';
 import '../../util/colors.dart';
+import '../../util/confirm_action.dart';
 
-class DrugDetails extends StatelessWidget {
+class DrugDetails extends ConsumerWidget {
   final DrugEntry drug;
   const DrugDetails(this.drug, {super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final locale = AppLocalizations.of(context)!.localeName;
+  Widget build(BuildContext context, WidgetRef ref) {
+    final drugStateNotifier = ref.read(drugEntriesProvider.notifier);
+    final localizations = AppLocalizations.of(context)!;
     final titleFontSize = Theme.of(context).textTheme.titleMedium!.fontSize;
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () => showConfirmAction(
+              context: context,
+              title: Text(localizations.drugDeleteConfirmTitle),
+              icon: const Icon(Icons.delete),
+              description: Text(localizations.drugDeleteConfirmDescription(
+                  drug.name, localizations.delete.toLowerCase())),
+              confirmText: Text(localizations.delete),
+              onConfirm: () {
+                drugStateNotifier.delete(drug);
+                showDrugDeleteUndoSnackbar(
+                  context: context,
+                  drug: drug,
+                  onUndo: () => drugStateNotifier.add(drug),
+                  localizations: localizations,
+                );
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ],
         title: Row(
           children: [
             Container(
@@ -47,7 +74,7 @@ class DrugDetails extends StatelessWidget {
             Text(drug.amount),
             const SizedBox(height: 8),
             const Text('Datum:', style: TextStyle(fontWeight: FontWeight.bold)),
-            Text(formatDateTime(drug.date, locale)),
+            Text(formatDateTime(drug.date, localizations.localeName)),
             const SizedBox(height: 8),
             const Text('Notities:',
                 style: TextStyle(fontWeight: FontWeight.bold)),
